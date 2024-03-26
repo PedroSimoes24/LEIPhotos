979e6465795cca28832d5ed24f5bc93b2b666f81
Pedro Simões <fc59822@alunos.fc.ul.pt>
2024-03-26 19:34:20 +0000

    JpegMetadataException and basic JavaXTMetadataReaderAdapter structure

---
+++ src/leiphotos/domain/metadatareader/JavaXTMetadataReaderAdapter.java
@@ -0,0 +1,27 @@
+public class JavaXTMetadataReaderAdapter implements JavaXTJpegMetadataReader {
+
+    public JavaXTMetadataReaderAdapter(File file) {
+        //TODO
+    }
+
+    public String getCamera() {
+        //TODO
+    }
+
+    public String getManufacturer() {
+        //TODO
+    }
+
+    public LocalDateTime getDate() {
+        //TODO
+    }
+
+    public String getAperture() {
+        //TODO
+    }
+
+    public double[] getGpsLocation()  {
+        //TODO
+    }
+
+}
---
+++ src/leiphotos/domain/metadatareader/JpegMetadataException.java
@@ -0,0 +1,6 @@
+public class JpegMetadataException extends Exception {
+
+    public JpegMetadataException() {
+        super("Unable to load metadata resource");
+    }
+}
