5d193b835ba20fb5ca7e9e389955fabd891a8b61
Pedro Simões <fc59822@alunos.fc.ul.pt>
2024-04-15 20:53:57 +0100

    bug fixes in AAlbum and Album

--- src/leiphotos/domain/albums/AAlbum.java
+++ src/leiphotos/domain/albums/AAlbum.java
@@ -4,28 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import leiphotos.domain.core.LibraryEvent;
 import leiphotos.domain.facade.IPhoto;
 
-public class AAlbum {
+public abstract class AAlbum implements IAlbum{
 
     String name;
 
     List<IPhoto> photos;
 
-    public AAlbum (String name) {
+    protected AAlbum (String name) {
 
         this.name = name;
         this.photos = new ArrayList<>();
     }
 
+    @Override
+    public int numberOfPhotos() {
+        return photos.size();
+    }
+
+    @Override
     public String getName() {
         return this.name;
     }
     
+    @Override
     public List<IPhoto> getPhotos() {
         return this.photos;
     }
 
+    @Override
     public boolean addPhotos(Set<IPhoto> selectedPhotos) {
 
         boolean added = false;
@@ -41,6 +50,7 @@ public class AAlbum {
         return added;
     }
 
+    @Override
     public boolean removePhotos(Set<IPhoto> selectedPhotos) {
 
         boolean removed = false;
@@ -54,4 +64,10 @@ public class AAlbum {
         }
         return removed;
     }
+
+    @Override
+    public void processEvent(LibraryEvent e) {
+        
+    }
+    
 }
--- src/leiphotos/domain/albums/Album.java
+++ src/leiphotos/domain/albums/Album.java
@@ -5,4 +5,5 @@ public class Album extends AAlbum{
     public Album(String name) {
         super(name);
     }
+    
 }
