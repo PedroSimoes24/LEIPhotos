1eda35098d1ac4aa196851afebb92f7f5b8520f3
Pedro Simões <fc59822@alunos.fc.ul.pt>
2024-04-15 21:49:34 +0100

    implemented IAlbumCatalog e AlbumCatalog

--- src/leiphotos/domain/albums/AlbumsCatalog.java
+++ src/leiphotos/domain/albums/AlbumsCatalog.java
@@ -1,13 +1,92 @@
 package leiphotos.domain.albums;
 
-import leiphotos.domain.core.MainLibrary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-//Class automatically generated so the code compiles
-//CHANGE ME
-public class AlbumsCatalog extends IAlbumsCatalog {
+import leiphotos.domain.core.MainLibrary;
+import leiphotos.domain.facade.IPhoto;
+
+
+public class AlbumsCatalog implements IAlbumsCatalog {
+
+	Map<String, IAlbum> catalog; 
+	MainLibrary mainLib;
 
 	public AlbumsCatalog(MainLibrary mainLib) {
-		// TODO Auto-generated constructor stub
+
+		this.mainLib = mainLib;
+		this.catalog = new HashMap<>();
+	}
+
+	@Override
+	public boolean createAlbum(String albumName) {
+		
+		boolean added = false;
+
+		if (!containsAlbum(albumName)) {
+
+			IAlbum a = new Album(albumName);
+			catalog.put(albumName, a);
+			added = true;
+		}
+
+		return added;
+	}
+
+	@Override
+	public boolean deleteAlbum(String albumName) {
+		
+		boolean removed = false;
+
+		if (containsAlbum(albumName)) {
+			catalog.remove(albumName);
+			removed = true;
+		}
+
+		return removed;
+	}
+
+	@Override
+	public boolean containsAlbum(String albumName) {
+		return catalog.containsKey(albumName);
+	}
+
+	@Override
+	public boolean addPhotos(String albumName, Set<IPhoto> selectedPhotos) {
+		
+		boolean added = false;
+
+		if (containsAlbum(albumName)) {
+			catalog.get(albumName).addPhotos(selectedPhotos);
+			added = true;
+		}
+
+		return added;
+	}
+
+	@Override
+	public boolean removePhotos(String albumName, Set<IPhoto> selectedPhotos) {
+
+		boolean removed = false;
+
+		if (containsAlbum(albumName)) {
+			catalog.get(albumName).removePhotos(selectedPhotos);
+			removed = true;
+		}
+
+		return removed;
+	}
+
+	@Override
+	public List<IPhoto> getPhotos(String albumName) {
+		return catalog.get(albumName).getPhotos();
+	}
+
+	@Override
+	public Set<String> getAlbumsNames() {
+		return catalog.keySet();
 	}
 
 }
--- src/leiphotos/domain/albums/IAlbumsCatalog.java
+++ src/leiphotos/domain/albums/IAlbumsCatalog.java
@@ -1,6 +1,23 @@
 package leiphotos.domain.albums;
-//Class automatically generated so the code compiles
-//CHANGE ME
-public class IAlbumsCatalog {
 
+import java.util.List;
+import java.util.Set;
+
+import leiphotos.domain.facade.IPhoto;
+
+public interface IAlbumsCatalog {
+
+    boolean createAlbum(String albumName);
+
+    boolean deleteAlbum(String albumName);
+
+    boolean containsAlbum(String albumName);
+
+    boolean addPhotos(String albumName, Set<IPhoto> selectedPhotos);
+
+    boolean removePhotos(String albumName, Set<IPhoto> selectedPhotos);
+
+    List<IPhoto> getPhotos(String albumName);
+
+    Set<String> getAlbumsNames();
 }
