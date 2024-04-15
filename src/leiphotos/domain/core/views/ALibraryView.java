359bb08f96b785132f02c1778e3905eb56daa79c
Pedroc20 <fc59791@alunos.fc.ul.pt>
2024-04-16 00:34:09 +0100

    implemented ALibraryView.java

--- src/leiphotos/domain/core/views/ALibraryView.java
+++ src/leiphotos/domain/core/views/ALibraryView.java
@@ -1,7 +1,13 @@
 package leiphotos.domain.core.views;
 
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.junit.runner.OrderWithValidator;
 
 import leiphotos.domain.core.Library;
 import leiphotos.domain.facade.IPhoto;
@@ -9,38 +15,41 @@ import leiphotos.domain.facade.IPhoto;
 public abstract class ALibraryView implements ILibraryView {
 
     private Library lib;
+    private Predicate<IPhoto> condition;
     private Comparator<IPhoto> criteria;
+    private List<IPhoto> view;
 
-    public ALibraryView(List<Photo> photos) {
+    protected ALibraryView(Library lib, Predicate<IPhoto> condition) {
+        this.lib = lib;
+        this.condition = condition;
+        this.criteria = (p1,p2) -> Long.compare(p1.size(), p2.size());
 
+        view = lib.getPhotos().stream()
+                  .filter(condition)
+                  .collect(Collectors.toList());
+
+        view.sort(criteria);
     }
 
     @Override
     public List<IPhoto> getMatches(String regexp) {
-        // TODO Auto-generated method stub
-        return null;
+        return view.stream().filter(p -> p.matches(regexp))
+                            .collect(Collectors.toList());
     }
 
     @Override
     public List<IPhoto> getPhotos() {
-        // TODO Auto-generated method stub
-        return null;
+        return view;
     }
 
     @Override
     public int numberOfPhotos() {
-        // TODO Auto-generated method stub
-        return 0;
+        return view.size();
     }
 
     @Override
     public void setComparator(Comparator<IPhoto> c) {
-        // TODO Auto-generated method stub
-        
+        criteria = c;
     }
-
-
-
-
 
 }
